a72044d3ee26b61c6df3aaf77ebd1b77d6c31b7b
Hernan Metaute <hmetaute@cvent.com>
2019-08-29 12:37:10 -0500

    Trying to ignore emacs created files

--- init_database.sh
+++ init_database.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+DEV_PWD=${1:-devpwd}
+echo 'pwd' $DEV_PWD
+
 psql -h localhost -U postgres -p 5432 << EOF
 CREATE DATABASE mantarrayas;
-CREATE USER schema_user WITH PASSWORD 'devpwd';
+CREATE USER schema_user WITH PASSWORD $DEV_PWD;
 GRANT ALL PRIVILEGES ON DATABASE "mantarrayas" to schema_user; \
 GRANT ALL ON SCHEMA public TO schema_user; \
 GRANT ALL ON ALL TABLES IN SCHEMA public TO schema_user;
